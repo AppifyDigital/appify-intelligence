81e31a3d6c93bf643d8560e5b7af67b15b751d49
Milutin-P <milutin.pesikan@gmail.com>
2025-10-10 13:38:36 +0200

    Update next.config.js to set globalObject for webpack, modify package.json to separate dev and devt scripts, and add favicon.ico to public directory.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.output.globalObject = `(typeof self !== 'undefined' ? self : this)`;
+    return config;
+  },
   images: {
     domains: ["images.unsplash.com"],
   },
